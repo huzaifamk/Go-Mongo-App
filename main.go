e438f82279a4e4da974c33b1258858bc649db18f
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-07-15 22:52:21 +0500

    C

--- main.go
+++ main.go
@@ -3,10 +3,29 @@ package main
 import (
 	"fmt"
 
+	"github.com/huzaifamk/Go-Mongo-App/controllers"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
 	fmt.Println("Go-Mongo-App")
 	e := echo.New()
+	e.Use(middleware.Logger())
+	uc := controllers.NewSubjectController(getSession())
+	e.GET("/subjects", uc.GetAll)
+	e.POST("/subjects", uc.Create)
+	e.GET("/subjects/:id", uc.GetOne)
+	e.PUT("/subjects/:id", uc.Update)
+	e.DELETE("/subjects/:id", uc.Delete)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getSession() *mgo.Session {
+	session, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	return session
 }
